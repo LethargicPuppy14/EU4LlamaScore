614d70a6d6ec01de9a84870949452c8e99f69627
LethargicPuppy14 <71605167+LethargicPuppy14@users.noreply.github.com>
2022-05-06 16:46:20 -0500

    Has a proper file chooser. Also runs infinitely

--- Java/ScoreCalculator.java
+++ Java/ScoreCalculator.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.io.File;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class ScoreCalculator{
-    private File savedGame = new File("R:\\Documents\\Paradox Interactive\\Europa Universalis IV\\save games\\MP\\Britain 1614 uncompressed.eu4");
+public class ScoreCalculator extends JFrame{
+    //private File savedGame = new File("R:\\Documents\\Paradox Interactive\\Europa Universalis IV\\save games\\MP\\Britain 1614 uncompressed.eu4");
+    private File savedGame;
     private Scanner readFile;
     private ArrayList<Player> playerList;
 
@@ -17,7 +19,7 @@ public class ScoreCalculator{
     }
 
     public ScoreCalculator(){
-        //File savedGame = loadFile();
+        savedGame = loadFile();
         playerList = new ArrayList<Player>();
 
         try{
@@ -67,7 +69,7 @@ public class ScoreCalculator{
                 String country = removeQuotes(readFile.nextLine());
                 //System.out.println("\t Country: " + country);
                 playerList.add(new Player(player, country));
-                
+
                 //To prevent infinite loops. No eu4 save file is 10,000,000 lines long, right?
                 if(counter > 10000000) break;
             }
@@ -88,7 +90,12 @@ public class ScoreCalculator{
         JFileChooser fileChooser = new JFileChooser();
         FileNameExtensionFilter filter = new FileNameExtensionFilter("EU4", "eu4");
         fileChooser.setFileFilter(filter);
-        //int returnVal = fileChooser.showOpenDialog(ScoreCalculator.this);
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        //TODO: Try to set this to the eu4 saves directory, or at least the documents folder
+        int result = fileChooser.showOpenDialog(this);
+        if(result == JFileChooser.APPROVE_OPTION){
+            return fileChooser.getSelectedFile();
+        }
         return null;
     }
 
